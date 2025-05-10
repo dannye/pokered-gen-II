07d565792641b8437158ff98133309e54c2e24e1
CreamElDudJafar <jahern2398@gmail.com>
2025-05-10 16:10:06 -0500

    Fix Status Screen
    Remove small tile on status screen

--- engine/pokemon/status_screen.asm
+++ engine/pokemon/status_screen.asm
@@ -105,11 +105,11 @@ StatusScreen:
 	push af
 	xor a
 	ldh [hTileAnimations], a
-	hlcoord 19, 1
-	lb bc, 6, 10
+	hlcoord 19, 3
+	lb bc, 2, 8
 	call DrawLineBox ; Draws the box around name, HP and status
-	ld de, -6
-	add hl, de
+	hlcoord 2, 7
+	nop
 	ld [hl], "<DOT>"
 	dec hl
 	ld [hl], "№"
@@ -313,8 +313,9 @@ StatusScreen2:
 	hlcoord 9, 2
 	lb bc, 5, 10
 	call ClearScreenArea ; Clear under name
-	hlcoord 19, 3
-	ld [hl], $78
+	hlcoord 19, 1
+	lb bc, 6, 10
+	call DrawLineBox ; Draws the box around the name, HP and status
 	hlcoord 0, 8
 	ld b, 8
 	ld c, 18
